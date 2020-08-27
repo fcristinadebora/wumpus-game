d042887296832690e5a0f4d764527eace610cfdd
Debora Cristina <fcristinadebora@gmail.com>
2020-08-26 22:30:16 -0300

    first commit - começando a construir o tabuleiro

---
+++ src/components/Pagina.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container-fluid text-center pt-5">
+    <div class="col-12">
+      <div class="row">
+        <div class="col-md-4">
+          <h1>Bem-vindo ao Mundo Wumpus</h1>
+          <ul class="text-left">
+            <li>Use as teclas de direção para mover seu personagem.</li>
+            <li>Pressione a barra de espaço para atirar a flexa</li>
+            <li>
+              Você só pode se mover para a esquerda/direita. Movimentos
+              diagonais não são permitidos
+            </li>
+            <li>
+              Cuidado para não ser comido pelo wumpus nem cair nos buracos
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-md-8 d-flex justify-content-center">
+          <tabuleiro></tabuleiro>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Tabuleiro from "@/components/Tabuleiro";
+export default {
+  components: {
+    Tabuleiro,
+  },
+};
+</script>
---
+++ src/components/Tabuleiro.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <table class="tabuleiro margin-auto">
+      <tr>
+        <td class="comVento">
+            
+        </td>
+        <td>
+            <img src="@/assets/img/buraco.png" alt="Ouro" style="width:130px">
+        </td>
+        <td class="comVento"></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td class="comVento">
+            <img src="@/assets/img/gold.gif" alt="Ouro" style="width:100px">
+        </td>
+        <td>
+            <img src="@/assets/img/semflecha.png" alt="Ouro" style="height:100px" v-if="false">
+            <img src="@/assets/img/comflecha.png" alt="Ouro" style="height:100px">
+        </td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td class="comFedor">
+        </td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>
+            
+        </td>
+        <td class="comFedor">
+        </td>
+        <td>
+            <img src="@/assets/img/wumpus.gif" alt="Wumpus" style="width:80px">
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+table.tabuleiro {
+  td {
+    width: 130px;
+    height: 130px;
+    background: url('../assets/img/terra.jpg') 100%;
+    background-size: 100%;
+    border: 5px solid #ccc;
+
+    }
+
+    .comVento{
+        background: url('../assets/img/vento.png') 100% no-repeat, url('../assets/img/terra.jpg');
+        background-size: 100%;
+    }
+    .comFedor{
+        background: url('../assets/img/fedor.png') 100% no-repeat, url('../assets/img/terra.jpg');
+        background-size: 100%;
+    }
+}
+</style>
